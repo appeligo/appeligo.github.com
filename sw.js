9b7b911a1088a98f24090b6dbbca539278881a80
Dirck Hecking <dhecking@gmail.com>
2019-04-27 07:58:42 -0400

    dumb service worker

--- sw.js
+++ sw.js
@@ -1,50 +1,12 @@
 
-console.log('Script loaded!')
-var cacheStorageKey = 'appeligo-pwa-1'
-
-var cacheList = [];
-
 self.addEventListener('install', function(e) {
-  console.log('Cache event!')
-  e.waitUntil(
-    caches.open(cacheStorageKey).then(function(cache) {
-      console.log('Adding to Cache:', cacheList)
-      return cache.addAll(cacheList)
-    }).then(function() {
-      console.log('Skip waiting!')
-      return self.skipWaiting()
-    })
-  )
+  console.log('install')
 })
 
 self.addEventListener('activate', function(e) {
-  console.log('Activate event')
-  e.waitUntil(
-    Promise.all(
-      caches.keys().then(cacheNames => {
-        return cacheNames.map(name => {
-          if (name !== cacheStorageKey) {
-            return caches.delete(name)
-          }
-        })
-      })
-    ).then(() => {
-      console.log('Clients claims.')
-      return self.clients.claim()
-    })
-  )
+  console.log('activate')
 })
 
 self.addEventListener('fetch', function(e) {
-  // console.log('Fetch event:', e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      if (response != null) {
-        console.log('Using cache for:', e.request.url)
-        return response
-      }
-      console.log('Fallback to fetch:', e.request.url)
-      return fetch(e.request.url)
-    })
-  )
+  console.log('fetch')
 })
