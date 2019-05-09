301e6b4dfcad803a33d9cbf6377316364a705d9c
Dirck Hecking <dirck@appeligo.com>
2019-05-09 03:36:24 -0400

    omg overhaul

---
+++ partners/google/js/credentials.js
@@ -0,0 +1,10 @@
+// Enter an API key from the Google API Console:
+//   https://console.developers.google.com/apis/credentials
+var apiKey = 'AIzaSyCxA3hQbxOt34KyxmbLAtOBPKkPxyilosA';
+
+
+// Enter a client ID for a web application from the Google API Console:
+//   https://console.developers.google.com/apis/credentials?project=_
+// In your API Console project, add a JavaScript origin that corresponds
+//   to the domain where you will be running the script.
+var clientId = '814810282938-gnnaum7gsgq78l3vb8tkrr3jmq5u5bhn.apps.googleusercontent.com';
